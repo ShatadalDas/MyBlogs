4b779dca0e2c6be6f919e9590fe960ab4872fa2f
ShatadalDas <shatadaldas2020@gmail.com>
2022-12-25 22:47:44 +0530

    Adding Google Search Console

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,18 @@ import "./_styles/globals.scss";
 import "../components/globals.scss";
 import "../utils/components/styles/globals.scss";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="48qvYpzsAXb5Uy2-GinHDprntgoEin-EU98SvXyj9wQ"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
