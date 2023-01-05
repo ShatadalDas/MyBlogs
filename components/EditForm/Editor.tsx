7f28a0328c712524be639ed093416adfc382b1ad
ShatadalDas <shatadaldas2020@gmail.com>
2023-01-05 10:27:52 +0530

    Added Monco editor

--- components/EditForm/Editor.tsx
+++ components/EditForm/Editor.tsx
@@ -1,28 +1,39 @@
 import React, { Dispatch } from "react";
 import { ActionType, BlogDataType } from "../../pages/admin/edit";
+import MonacoEditor from "@monaco-editor/react";
+import * as monaco from "monaco-editor";
 
 type Props = {
   content: string;
   dispatch: Dispatch<ActionType<Partial<BlogDataType>>>;
 };
 
+const MONACO_OPTIONS: monaco.editor.IEditorConstructionOptions = {
+  minimap: {
+    enabled: false,
+  },
+  wordWrap: "on",
+  lineHeight: 1.7,
+  links: true,
+  mouseWheelZoom: true,
+};
+
 function Editor({ content, dispatch }: Props) {
   return (
-    <div className="editor">
-      <label htmlFor="blgCnt" className="sr-only">
-        Blog Content
-      </label>
-      <textarea
-        className="editor__textarea"
-        id="blgCnt"
-        placeholder="write the content in markdown language"
-        onChange={(e) =>
-          dispatch({ type: "update", payload: { content: e.target.value } })
-        }
-        value={content}
-        spellCheck={false}
-      />
-    </div>
+    <MonacoEditor
+      className="editor"
+      language="markdown"
+      theme="vs-dark"
+      onChange={(val) =>
+        dispatch({ type: "update", payload: { content: val } })
+      }
+      value={content}
+      defaultValue="<!-- write the content in markdown language -->"
+      width="50%"
+      height="100%"
+      
+      options={MONACO_OPTIONS}
+    />
   );
 }
 
