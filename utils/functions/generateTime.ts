2ce3a3e7a808efd36c9f6ab4bccb6cd3b84c08d4
ShatadalDas <shatadaldas2020@gmail.com>
2023-01-05 10:41:02 +0530

    Optimized

--- utils/functions/generateTime.ts
+++ utils/functions/generateTime.ts
@@ -1,16 +1,14 @@
 export default function generateTime(): string {
   const dateObj = new Date();
 
-  const date = dateObj.getDate();
-  const month = dateObj.getMonth() + 1;
-  const year = dateObj.getFullYear();
+  const date = putZero(dateObj.getDate());
+  const month = putZero(dateObj.getMonth() + 1);
+  const year = putZero(dateObj.getFullYear());
 
-  const hh = dateObj.getHours();
-  const mm = dateObj.getMinutes();
+  const hh = putZero(dateObj.getHours());
+  const mm = putZero(dateObj.getMinutes());
 
-  return `${putZero(date)}-${putZero(month)}-${year} ${putZero(hh)}:${putZero(
-    mm
-  )} (GMT +05:30)`;
+  return `${date}-${month}-${year} ${hh}:${mm} (GMT +05:30)`;
 }
 
 function putZero(val: number) {
