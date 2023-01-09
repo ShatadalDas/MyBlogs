086812ab2347f2a2f3732ecc61eee6ebae179610
ShatadalDas <shatadaldas2020@gmail.com>
2023-01-09 14:27:11 +0530

    Some changes

--- utils/functions/createUrl.ts
+++ utils/functions/createUrl.ts
@@ -5,14 +5,21 @@ export default function createUrl(title: string): string {
   let words = title.split(" ");
 
   let res = "";
+
+  //* looping through all the words of the title
   for (let i = 0; i < words.length; i++) {
+    //* Taking one word
     const word = words[i];
+
+    //* Looping through all the characters of the word
     for (let j = 0; j < word.length; j++) {
+      //* Taking one character
       const ch = word[j];
-      res += isAlphaNum(ch) ? ch.toLowerCase() : charToHex(ch);
+      
+      res += isAlphaNum(ch) ? ch.toLowerCase() : toHexaDecimal(ch);
     }
 
-    res += i === words.length - 1 ? "" : "-";
+    if (i !== words.length - 1) res += "-";
   }
   return res;
 }
@@ -21,7 +28,7 @@ function isAlphaNum(char: string) {
   return /^[a-zA-Z0-9]/.test(char);
 }
 
-function charToHex(ch: string) {
+function toHexaDecimal(ch: string) {
   let hex = "%" + Number(ch.charCodeAt(0)).toString(16).toUpperCase();
   return hex;
 }
