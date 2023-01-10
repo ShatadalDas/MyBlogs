c9507899a0847d4a951196ffeffd5cd9038895fc
ShatadalDas <shatadaldas2020@gmail.com>
2023-01-10 13:00:46 +0530

    Added font subset as latin

--- utils/hooks/useFont.ts
+++ utils/hooks/useFont.ts
@@ -28,30 +28,36 @@ const consolas = LocalFont({
 });
 
 const work_sans = Work_Sans({
+  subsets: ['latin'],
   variable: "--fnt-work-sans",
 });
 
 const ubuntu = Ubuntu({
+  subsets: ['latin'],
   weight: ["400", "300", "500", "700"],
   variable: "--fnt-ubuntu",
 });
 
 const lato = Lato({
+  subsets: ['latin'],
   weight: ["100", "300", "400", "700", "900"],
   variable: "--fnt-lato",
 });
 
 const roboto = Roboto({
+  subsets: ['latin'],
   weight: ["100", "300", "400", "500", "700", "900"],
   variable: "--fnt-roboto"
 });
 
 const inria_serif = Inria_Serif({
+  subsets: ['latin'],
   weight: ["300", "400", "700"],
   variable: "--fnt-inria-serif"
 });
 
 const inconsolata = Inconsolata({
+  subsets: ['latin'],
   variable: "--fnt-inconsolata",
 });
 
